87cb2865a7253131c32085a8ff5d96d214e5cb84
Aziz Yokubjonov <aziz.yokubjonov@gmail.com>
2019-10-10 20:53:37 -0400

    use env

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,11 @@
 import * as functions from 'firebase-functions';
-import * as slackConfig from './slackConfig.json';
 const fetch = require('node-fetch');
 
 export const feedbackLogger = functions.database.ref('/feedback/{pushId}').onCreate((snapshot: any) => {
     const feedbackItem = snapshot.val();
     let message = `*${feedbackItem.name}* said "${feedbackItem.comment}" on page https://codelab.fun/${feedbackItem.href}`;
     message += feedbackItem.email ? `\nYou can get back to *${feedbackItem.name}* at ${feedbackItem.email}` : '';
-    return fetch(slackConfig.webhookURL, {
+    return fetch(functions.config().slack.url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
